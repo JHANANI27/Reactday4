81126294cbd0e1c3c3677ff9409f986120623285
JHANANI27 <108513771+JHANANI27@users.noreply.github.com>
2023-03-31 15:14:53 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import Fruit from './Components/Fruit';
+import './App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <center>
+          <Fruit/>
+        </center>
+      </div>
+    )
+  }
+}
+export default App
